6f4fcf07e562f12bf9539b01ac88baef2ffc01cb
Alexander Evsyuchenya <alexander.evsyuchenya@adms-imac-2.hiqo-solutions.com>
2015-09-09 16:35:19 +0300

    refactoring

---
+++ Classes/OSNetworkKit.h
@@ -0,0 +1,13 @@
+//
+//  OSNetworkKit.h
+//  OSNetworkKit
+//
+//  Created by Alexander Evsyuchenya on 8/11/15.
+//  Copyright (c) 2015 Orangesoft. All rights reserved.
+//
+
+#import "OSObjectManager.h"
+#import "OSRequestOperation.h"
+#import "OSRKRequestOperation.h"
+#import "OSRestKitOperationProtocol.h"
+#import "OSPaginableOperation.h"
